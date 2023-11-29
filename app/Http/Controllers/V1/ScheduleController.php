45e0e5b6eacbc2411108fe32aedab8200c0d274c
Lincoln-g20 <andrelincolnfs2014@gmail.com>
2023-11-29 12:53:32 -0300

    Created routes and controllers

---
+++ app/Http/Controllers/V1/ScheduleController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Schedule;
+use Illuminate\Http\Request;
+
+class ScheduleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {   
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Schedule $schedule)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Schedule $schedule)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/V1/StudyBlockController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\StudyBlock;
+use Illuminate\Http\Request;
+
+class StudyBlockController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StudyBlock $studyBlock)
+    {
+        // 
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, StudyBlock $studyBlock)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
