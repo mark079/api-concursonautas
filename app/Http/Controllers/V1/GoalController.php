17b12c1370eec43509bab7866c221c1fd531b77d
theuscruzz <matheuscruz211r@gmail.com>
2023-11-29 16:26:43 -0300

    Goal and Schedule CRUD and User get

--- app/Http/Controllers/V1/GoalController.php
+++ app/Http/Controllers/V1/GoalController.php
@@ -3,17 +3,22 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\GoalResource;
 use App\Models\Goal;
+use App\Traits\HttpResponses;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class GoalController extends Controller
 {
+    use HttpResponses;
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return response()->json(GoalResource::collection(Goal::all()));
     }
 
     /**
@@ -21,7 +26,29 @@ class GoalController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required|exists:users,id',
+            'type' => 'required|in:C,V',
+            'test_date' => ['required', 'date', 'date_format:Y-m-d', function ($attribute, $value, $fail) {
+                $dataRequisitada = Carbon::parse($value);
+
+                if ($dataRequisitada->lte(Carbon::now()->addWeek())) {
+                    $fail("Data deve ser no mínimo uma semana futura");
+                }
+            }],
+            'content_to_study' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error('Invalid Data', 422, $validator->errors());
+        }
+
+        $created = Goal::create($validator->validated());
+
+        if ($created) {
+            return $this->success('Registered Data', 200, new GoalResource($created));
+        }
+        return $this->error('Something went wrong', 400);
     }
 
     /**
@@ -29,7 +56,7 @@ class GoalController extends Controller
      */
     public function show(Goal $goal)
     {
-        //
+        return response()->json(new GoalResource($goal));
     }
 
     /**
@@ -37,7 +64,23 @@ class GoalController extends Controller
      */
     public function update(Request $request, Goal $goal)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required|exists:users,id',
+            'type' => 'required|in:C,V',
+            'test_date' => 'required|date|date_format:Y-m-d',
+            'content_to_study' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error('Invalid Data', 422, $validator->errors());
+        }
+
+        $updated = $goal->update($validator->validated());
+        if ($updated) {
+            return $this->success('Goal Updated', 200, new GoalResource($goal));
+        }
+
+        return $this->error('Something went wrong', 400);
     }
 
     /**
@@ -45,6 +88,14 @@ class GoalController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $goal = Goal::find($id);
+        if ($goal) {
+            $deleted = $goal->delete();
+            if ($deleted) {
+                return $this->success('Goal Deleted', 200);
+            }
+            return $this->error('Something went wrong', 400);
+        }
+        return $this->error('Goal Not Found', 404);
     }
 }
--- app/Http/Controllers/V1/ScheduleController.php
+++ app/Http/Controllers/V1/ScheduleController.php
@@ -3,17 +3,25 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\ScheduleResource;
 use App\Models\Schedule;
+use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class ScheduleController extends Controller
 {
+    use HttpResponses;
     /**
      * Display a listing of the resource.
      */
     public function index(Request $request)
     {
-        //
+        if (array_key_exists('goal', $request->query())) {
+            return response()->json(ScheduleResource::collection(Schedule::where([['goal_id', '=', $request->query()['goal']]])->orderBy('date', 'asc')->get()));
+        }
+        return response()->json(ScheduleResource::collection(Schedule::all()));
     }
 
     /**
@@ -21,7 +29,24 @@ class ScheduleController extends Controller
      */
     public function store(Request $request)
     {   
-        //
+        $validator = Validator::make($request->all(), [
+            '*.user_id' => 'required|exists:users,id',
+            '*.goal_id' => 'required|exists:goals,id',
+            '*.weekday' => 'required|integer|min:1|max:7',
+            '*.start_time' => 'required|date_format:H:i',
+            // 'end_time' => 'required|date_format:H:i|after:start_time'
+        ]);
+        
+        if ($validator->fails()) {
+            return $this->error('Invalid Data', 422, $validator->errors());
+        }
+        
+        $created = Schedule::insert($request->all());
+        if ($created) {
+            return $this->success("Registred Data", 200);
+        }
+
+        return $this->error('Something went wrong', 400);
     }
 
     /**
@@ -29,7 +54,7 @@ class ScheduleController extends Controller
      */
     public function show(Schedule $schedule)
     {
-        //
+        return response()->json(new ScheduleResource($schedule));
     }
 
     /**
@@ -37,7 +62,25 @@ class ScheduleController extends Controller
      */
     public function update(Request $request, Schedule $schedule)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required|exists:users,id',
+            'goal_id' => 'required|exists:goals,id',
+            'weekday' => 'required|integer|min:1|max:7',
+            'start_time' => 'required|date_format:H:i',
+            // 'end_time' => 'required|date_format:H:i|after:start_time'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error('Invalid Data', 400, $validator->errors());
+        }
+
+        $created = $schedule->update($validator->validated());
+
+        if ($created) {
+            return $this->success('Registred Data', 200, $schedule);
+        }
+
+        return $this->error('Something went wrong', 400);
     }
 
     /**
@@ -45,6 +88,14 @@ class ScheduleController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $schedule = Schedule::find($id);
+        if ($schedule) {
+            $deleted = $schedule->delete();
+            if ($deleted) {
+                return $this->success('Schedule Deleted', 200);
+            }
+            return $this->error('Something went wrong', 400);
+        }
+        return $this->error('Schedule Not Found', 404);
     }
 }
